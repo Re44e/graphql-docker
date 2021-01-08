c92a989fcfb56fa879327905c1e39183b3025b61
Jean Pita <52089289+Re44e@users.noreply.github.com>
2021-01-08 13:43:13 -0300

    Clean Code (Regra do escoteiro).

--- src/server.js
+++ src/server.js
@@ -1,13 +1,14 @@
 import {ApolloServer, PubSub } from 'apollo-server'
 import mongoose from 'mongoose'
+require('dotenv').config()
 
 function start({ typeDefs, resolvers}) {
 
-  const DB_HOST = process.env.DB_HOST || 'localhost';
-  const DB_PORT = process.env.DB_PORT || '27017'
+  const DB_HOST = process.env.DB_HOST;
+  const DB_PORT = process.env.DB_PORT;
 
-  const API_HOST = process.env.API_HOST || '127.0.0.1';
-  const SERVER_PORT = process.env.SERVER_PORT || '4000';;
+  const API_HOST = process.env.API_HOST;
+  const SERVER_PORT = process.env.SERVER_PORT;
 
   const pubsub = new PubSub(); /* Responsável por criar o túnel de web-socket */
   const server = new ApolloServer({ 
